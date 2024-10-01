f9b3d3d5ddbe46f8aa634196eab81532533d3f9f
umarr <umarr.developer@gmail.com>
2024-10-01 12:15:46 +0600

    added views for edit and delete cats

--- api/cats/view.py
+++ api/cats/view.py
@@ -15,3 +15,21 @@ router = APIRouter(prefix='/cats')
 async def post_cat(cat: CatCreateSchema,
                    session: AsyncSession = Depends(db.session_dependency)):
     return await CatsRepository.create(session, **cat.model_dump())
+
+
+@router.patch('/edit',
+              response_model=CatGetSchema,
+              status_code=status.HTTP_200_OK)
+async def edit_cat(cat: CatUpdateSchema,
+                   session: AsyncSession = Depends(db.session_dependency)):
+    return await CatsRepository.edit(session, **cat.model_dump())
+
+
+@router.delete('/delete',
+               status_code=status.HTTP_200_OK)
+async def delete_cat(cat: CatDeleteSchema,
+                     session: AsyncSession = Depends(db.session_dependency)):
+    await CatsRepository.delete(session, cat_id=cat.cat_id)
+    return {
+        'detail': 'success'
+    }
