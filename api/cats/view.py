2d88f8c4b3c48dd79c76fe58c3ef8082fe97fd7d
umarr <umarr.developer@gmail.com>
2024-10-01 12:06:54 +0600

    added view for create cats

--- api/cats/view.py
+++ api/cats/view.py
@@ -1,3 +1,17 @@
-import fastapi
+from fastapi import Depends, APIRouter, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
-router = fastapi.APIRouter(prefix='/cats')
+from api.cats.schemas import CatCreateSchema, \
+    CatUpdateSchema, CatGetSchema, CatDeleteSchema, CatDetailSchema
+from src.database import db
+from src.repositories import CatsRepository
+
+router = APIRouter(prefix='/cats')
+
+
+@router.post('/create',
+             response_model=CatCreateSchema,
+             status_code=status.HTTP_201_CREATED)
+async def post_cat(cat: CatCreateSchema,
+                   session: AsyncSession = Depends(db.session_dependency)):
+    return await CatsRepository.create(session, **cat.model_dump())
