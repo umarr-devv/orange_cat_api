4e0ae80bb9be0f4fb719dfd8d0f9b579bd5defe6
umarr <umarr.developer@gmail.com>
2024-10-01 10:15:07 +0600

    alembic init

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import fastapi
 import uvicorn
+from api import router
 
 app = fastapi.FastAPI()
+app.include_router(router)
 
 if __name__ == '__main__':
     uvicorn.run('app:app', reload=True, host='0.0.0.0', port=8000)
