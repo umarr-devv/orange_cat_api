94e42b79c1da286c90b9493d703ca09715fbd543
umarr <umarr.developer@gmail.com>
2024-10-01 12:55:22 +0600

    added views for create and gets breeds

--- api/breeds/view.py
+++ api/breeds/view.py
@@ -1,3 +1,28 @@
-import fastapi
+from fastapi import Depends, APIRouter
+from sqlalchemy.ext.asyncio import AsyncSession
 
-router = fastapi.APIRouter(prefix='/breeds')
+from api.breeds.schemas import BreedCreateSchema, BreedGetSchema, BreedCatsSchema
+from src.database import db
+from src.repositories import BreedsRepository
+
+router = APIRouter(prefix='/breeds')
+
+
+@router.post('/create',
+             response_model=BreedGetSchema)
+async def create_breed(breed: BreedCreateSchema,
+                       session: AsyncSession = Depends(db.session_dependency)):
+    return await BreedsRepository.create(session, **breed.model_dump())
+
+
+@router.get('/',
+            response_model=list[BreedGetSchema])
+async def get_all_breeds(session: AsyncSession = Depends(db.session_dependency)):
+    return await BreedsRepository.get_all(session)
+
+
+@router.get('/{breed_id}',
+            response_model=list[BreedCatsSchema])
+async def get_cats_by_breed_id(breed_id: int,
+                               session: AsyncSession = Depends(db.session_dependency)):
+    return await BreedsRepository.get_cats_by_breed_id(session, breed_id)
